758d29a1b3000efc5bf3b911eac4d53ff9b2ec4f
avideb24 <debavi24@gmail.com>
2024-11-19 10:50:59 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Blogs from './Components/Blogs/Blogs'
 import Bookmarks from './Components/Bookmarks/Bookmarks'
 import Header from './Components/Header/Header'
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
 
   const [bookmarks, setBookmarks] = useState([]);
@@ -11,16 +14,29 @@ function App() {
   const [readTime, setReadTime] = useState(0);
 
   const handleBookmarks = (blog) => {
-      // console.log('adding soon bm', blog);
-      const newBookmarks = [...bookmarks, blog]
-      setBookmarks(newBookmarks)
+    // console.log('adding soon bm', blog);
+    const newBookmarks = [...bookmarks, blog]
+    setBookmarks(newBookmarks)
+    toast.error('🦄 Wow so easy!', {
+      position: "top-right",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+      });
   };
 
   const handleReadTime = blog => {
-      // console.log('time added soon', blog);
-      const newReadTime = readTime + blog.reading_time;
-      setReadTime(newReadTime);
-      
+    // console.log('time added soon', blog);
+    const newReadTime = readTime + blog.reading_time;
+    setReadTime(newReadTime);
+
+    // removing bookmark after read
+    const remainingBookmark = bookmarks.filter(bookmark => bookmark.id !== blog.id);
+    setBookmarks(remainingBookmark);
   };
 
 
@@ -29,7 +45,9 @@ function App() {
       <div className='max-w-screen-xl mx-auto '>
         <Header></Header>
         <div className='flex gap-8'>
+        {/* blogs */}
           <Blogs handleBookmarks={handleBookmarks} handleReadTime={handleReadTime}></Blogs>
+          <ToastContainer />
           <Bookmarks bookmarks={bookmarks} readTime={readTime}></Bookmarks>
         </div>
       </div>
